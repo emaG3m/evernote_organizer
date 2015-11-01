b37c0db5f46177d5d4a5de64b9f22a640897cac3
ecwilliams <ecwilliams@constantcontact.com>
2015-11-01 13:54:26 -0500

    Add tag ActiveRecord model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031224741) do
+ActiveRecord::Schema.define(version: 20151101184432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20151031224741) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "update_sequence_num"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "guid"
+    t.string  "name"
+    t.integer "update_sequence_num"
   end
 
 end
